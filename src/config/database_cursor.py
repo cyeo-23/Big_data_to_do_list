d62e9e1062919da66ce06c9d814440a9cbfbcc27
Moussa YEO <caudanna-yeo@telecom-paris.fr>
2023-10-22 13:24:09 +0200

    fix form

--- src/config/database_cursor.py
+++ src/config/database_cursor.py
@@ -1,18 +1,21 @@
-"""This module start a connection to the MongoDB database and instantiate the client and db object"""
+"""This module start a connection to the MongoDB
+database and instantiate the client and db object"""
 import os
 from dotenv import load_dotenv
+from pymongo.mongo_client import MongoClient
 
 # Charge les variables d'environnement à partir du fichier .env
 load_dotenv()
-from pymongo.mongo_client import MongoClient
 
 # reccuperer le lien de l'URL atlas dans le fichier d'environment
 atlas = os.getenv("atlas_link")
 
+
 def get_db_connection(uri):
     """This fuction create connection to the MongoDB database"""
     client = MongoClient(uri)
-    return client                          
+    return client
+
 
 client = get_db_connection(atlas)
 db = client.get_database()
