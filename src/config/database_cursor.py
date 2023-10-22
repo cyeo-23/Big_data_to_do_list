ba788838635ca86f709eacadf71ede22aed123f5
Moussa YEO <caudanna-yeo@telecom-paris.fr>
2023-10-22 12:19:19 +0200

    database add docstring

--- src/config/database_cursor.py
+++ src/config/database_cursor.py
@@ -1,3 +1,4 @@
+"""This module start a connection to the MongoDB database and instantiate the client and db object"""
 import os
 from dotenv import load_dotenv
 
@@ -6,13 +7,14 @@ load_dotenv()
 from pymongo.mongo_client import MongoClient
 
 # reccuperer le lien de l'URL atlas dans le fichier d'environment
-uri = os.getenv("atlas_link")
+atlas = os.getenv("atlas_link")
 
-def get_db_connection():
+def get_db_connection(uri):
+    """This fuction create connection to the MongoDB database"""
     client = MongoClient(uri)
     return client                          
 
 
 
-client = get_db_connection()
+client = get_db_connection(atlas)
 db = client.get_database()
