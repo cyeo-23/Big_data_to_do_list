8ab9b42c1255e7de62a5764825aaa6ef8a7e9afc
Moussa YEO <caudanna-yeo@telecom-paris.fr>
2023-10-30 12:34:01 +0100

    rename pages

---
+++ src/pages/Tâches.py
@@ -0,0 +1,28 @@
+"""Module to display the tasks page in the Streamlit application."""
+import streamlit as st
+from utils.st_utils import footer, nav_page, disconnect
+
+
+def display_page():
+    """Display the Streamlit page for tasks."""
+    st.set_page_config(layout="wide", page_title="You do", page_icon="📜")
+    if st.sidebar.button("Deconnexion"):
+        disconnect()
+        nav_page("")
+    st.markdown(
+        """<style>.font {
+        font-size:35px ; font-family: 'Cooper Black'; color: white;}
+        </style>""",
+        unsafe_allow_html=True
+    )
+    st.markdown('<p class="font">Tasks</p>', unsafe_allow_html=True)
+    st.write(f"Connecté avec l'ID: {st.session_state.user.id}")
+
+    footer()
+
+
+if __name__ == "__main__":
+    if 'user' not in st.session_state:
+        nav_page("")
+    else:
+        display_page()
